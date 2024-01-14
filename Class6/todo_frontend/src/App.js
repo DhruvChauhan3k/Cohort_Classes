05d551ced6ba65a7e84a8d494bb7659372157915
Dhruv <dhruvchauhan3k@gmail.com>
2024-01-14 23:36:09 +0530

    week 6 bugs fixed

--- Class6/todo_frontend/src/App.js
+++ Class6/todo_frontend/src/App.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import { useState, useEffect } from "react";
+import { useState, memo } from "react";
 function App() {
   const [title,setTitle]=useState()
   const [desc,setDesc]=useState()
   const [todo,setTodo]=useState([])
-  useEffect(() => {
-    todo.map((e)=>{
-      <div>
-        {e.title}
-        {e.desc}
-      </div>
-      return null;
-    })
-   },[todo]);
   return (
     <div>
-       Title:<input onChange={(e)=>{
-        setTitle(e.target.value)
-       }}></input><span></span>  
-       Description:<input onChange={(e)=>{
-        setDesc(e.target.value)
-       }}></input><span></span>
-       <button onClick={()=>{
-        setTodo([...todo, { title: title, desc: desc }]);
-       }}> Create</button>     
+         <input placeholder='Title' style={{margin:'10px',}} onChange={(e)=>{
+          setTitle(e.target.value);
+         }}></input>
+         <input placeholder='Description' style={{margin:'10px',}} onChange={(e)=>{
+          setDesc(e.target.value);
+         }}></input>        
+         <button onClick={(e)=>{
+          setTodo([...todo,{title:title,desc:desc}])
+         }}>Add</button>
+         <Render todo={todo} setTodo={setTodo}></Render>
     </div>
   );
 }
+
+
+const Render = memo(({ todo,setTodo }) => {
+
+  function Delete(index)
+{
+  const updatedTodo = todo.filter((_, i) => i !== index);
+  setTodo(updatedTodo)
+}
+
+  return (
+    <div>
+      {todo.map((t, index) => (
+        <div key={index}>
+          <li style={{margin:'10px'}}>{t.title} <button style={{margin:'10px'}} onClick={()=>Delete(index)}>Done</button></li>
+          <p style={{margin:'30px'}}>{t.desc}</p>
+          
+        </div>
+      ))}
+    </div>
+  );
+});
+
 
 export default App;
