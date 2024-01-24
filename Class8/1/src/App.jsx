f5d07ffe9aee98e72550c881fc594f336590c790
Dhruv <dhruvchauhan3k@gmail.com>
2024-01-24 23:21:02 +0530

    week 8.1

---
+++ Class8/1/src/App.jsx
@@ -0,0 +1,30 @@
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-start m-2	">
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none ">Default</button>
+  
+{/* //// */}
+
+
+
+
+  <div class="relative  flex w-full flex-wrap items-stretch">
+    <input
+      type="search"
+      class="relative m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+      placeholder="Search"
+      aria-label="Search"
+      aria-describedby="button-addon1" />
+  </div>
+
+
+
+</div>
+
+  
+  )
+}
+
+export default App
